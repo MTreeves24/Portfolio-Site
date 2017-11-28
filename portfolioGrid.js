be067cc86f90e1ec3b3d94bed7e30a05df656b09
Mike Treeves <mtreeves24@gmail.com>
2017-11-28 16:48:34 +0000

    new skills section and general UI improvements

--- portfolioGrid.js
+++ portfolioGrid.js
@@ -16,3 +16,31 @@ $('.menu-toggle').click(function() {
 });
 
 
+//SMOOTH SCROLLING//
+
+// Select all links with hashes
+$('a[href*="#"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          }
+        });
+      }
+    }
+  });
