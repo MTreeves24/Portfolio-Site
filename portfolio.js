aa252c2eee64c6c73823ff1ca77dd1ce40fc21ab
Mike Treeves <mtreeves24@gmail.com>
2020-03-30 11:58:28 +0100

    complete redesign of portfolio section - still in progress

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,13 @@
-function wait(ms=0){
+function wait(ms = 0) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function getRandomBetween(min = 50, max = 150, randomNumber = Math.random()){
+function getRandomBetween(min = 50, max = 150, randomNumber = Math.random()) {
   return Math.floor(randomNumber * (max - min) + min);
 }
 
 //Typed-Effect Text
-async function draw (el) {
+async function draw(el) {
   const text = el.textContent;
   let soFar = "";
   for (const letter of text) {
@@ -16,37 +16,41 @@ async function draw (el) {
     // wait for some amount of time
     const { typeMin, typeMax } = el.dataset;
     const waitTime = getRandomBetween(typeMin, typeMax);
-    await wait(waitTime)
+    await wait(waitTime);
   }
 }
 
 document.querySelectorAll(".title").forEach(draw);
 
-//Code to turn navbar opaque on scroll
-var nav = document.querySelector('nav'); 
+// code to turn navbar opaque on scroll
+var nav = document.querySelector("nav");
 
-window.addEventListener('scroll', function(event) { 
-    event.preventDefault();
+window.addEventListener("scroll", function(event) {
+  event.preventDefault();
 
-    if (window.scrollY >= 450) { 
-        nav.classList.add("solid-navbar");
-        nav.classList.remove("opaque-navbar");
-    } else {
-        nav.classList.add("opaque-navbar");
-        nav.classList.remove("solid-navbar");
-    }
+  if (window.scrollY >= 450) {
+    nav.classList.add("solid-navbar");
+    nav.classList.remove("opaque-navbar");
+  } else {
+    nav.classList.add("opaque-navbar");
+    nav.classList.remove("solid-navbar");
+  }
 });
 
-//Code to open and close hamburger menu
+// code to open and close hamburger menu
 const hamburger = document.querySelector(".menu-toggle");
 const hamburgerItem = document.querySelectorAll(".hamburger-item");
 
-
-
 var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 if (viewportWidth < 679) {
-	hamburgerItem.forEach(x => x.addEventListener("click", () => document.querySelector("ul").classList.toggle("opening")))
-  hamburger.addEventListener("click", () => document.querySelector("ul").classList.toggle("opening"))
+  hamburgerItem.forEach(x =>
+    x.addEventListener("click", () =>
+      document.querySelector("ul").classList.toggle("opening")
+    )
+  );
+  hamburger.addEventListener("click", () =>
+    document.querySelector("ul").classList.toggle("opening")
+  );
 } else {
-	console.log('Small viewport');
+  console.log("Small viewport");
 }
